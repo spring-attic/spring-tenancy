ba61a72a612fc1c6b60410939a4088c9738239fe
David Green <david.green@tasktop.com>
2011-02-16 09:32:16 -0800

    ASSIGNED - task 3: TenantAwareDatasource should enable extensibility for computing database name http://code2cloud.tasktop.com/alm/#projects/spring-tenancy/task/3

--- src/main/java/org/springframework/tenancy/datasource/TenantAwareDataSource.java
+++ src/main/java/org/springframework/tenancy/datasource/TenantAwareDataSource.java
@@ -1,15 +1,34 @@
 package org.springframework.tenancy.datasource;
 
+import org.springframework.tenancy.context.TenancyContext;
 import org.springframework.tenancy.context.TenancyContextHolder;
+import org.springframework.tenancy.core.Tenant;
 
+/**
+ * A data source that is tenant aware, that switches the database name based on the current tenant.
+ * 
+ * @author Clint Morgan
+ * @author David Green
+ */
 public class TenantAwareDataSource extends AbstractDatabaseSwitchingDataSource {
 
 	@Override
 	protected String getDatabaseName() {
 		String dbName = null;
-		if (TenancyContextHolder.getContext() != null && TenancyContextHolder.getContext().getTenant() != null) {
-			dbName = TenancyContextHolder.getContext().getTenant().getIdentity().toString();
+		TenancyContext tenancyContext = TenancyContextHolder.getContext();
+		if (tenancyContext.getTenant() != null) {
+			dbName = computeDatabaseName(tenancyContext.getTenant());
 		}
 		return dbName;
+	}
+
+	/**
+	 * Compute the database name based on a tenant.  The default implementation uses the {@link Tenant#getIdentity() tenant identity}
+	 * as the database name.
+	 * @param tenant the tenant, must not be null
+	 * @return the database name
+	 */
+	protected String computeDatabaseName(Tenant tenant) {
+		return tenant.getIdentity().toString();
 	}
 }
