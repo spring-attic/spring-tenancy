5766bc386fc79bcafbb7eb1818701d6aba32e9b8
Dave Syer <dsyer@vmware.com>
2011-02-16 09:16:03 +0000

    Tidy meta data and re-org test

--- src/test/java/org/springframework/tenancy/datasource/DatabaseSwitchingDataSourceTest.java
+++ src/test/java/org/springframework/tenancy/datasource/DatabaseSwitchingDataSourceTest.java
@@ -1,7 +1,5 @@
 package org.springframework.tenancy.datasource;
 
-import java.sql.SQLException;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -10,14 +8,20 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "/applicationContext-testDBSwitching.xml" })
-@Transactional()
+@ContextConfiguration
+@Transactional
+@DirtiesContext
 public class DatabaseSwitchingDataSourceTest {
 
 	@PersistenceContext
@@ -27,50 +31,47 @@ public class DatabaseSwitchingDataSourceTest {
 	private BasicDataSource dataSource;
 
 	@Autowired
+	private PlatformTransactionManager transactionManager;
+
+	@Autowired
 	private ThreadlocalDatabaseSwitchingDataSource switchingDataSource;
+
+	private static boolean initialized;
 
 	private final String DB1 = "DB1";
 	private final String DB2 = "DB2";
 
 	private final String CREATE_TABLE_STMT = "CREATE TABLE DOMAINOBJECT (ID INTEGER PRIMARY KEY, STRING CHAR(50)) GRANT ALL ON DOMAINOBJECT TO SA;";
 
-	// I don't really want this to be a test, just a before that does not run under a transaction, however, I can't make
-	// that work (@BeforeTransaction). This means that we must run all tests in the class.
-	@Rollback(false)
-	@Test
-	public void setupDB1() throws SQLException {
-		dataSource.getConnection().createStatement().execute("create schema " + DB1 + " " + CREATE_TABLE_STMT);
+	@BeforeTransaction
+	public void setUpDatabases() throws Exception {
 
-		switchingDataSource.setDatabaseName(DB1);
-		DomainObject do1 = getMockDomainObject("db1");
-		entityManager.persist(do1);
-		DomainObject result = (DomainObject) entityManager.createQuery("select do from DomainObject do;")
-				.getResultList().get(0);
-		Assert.assertNotNull(result);
-		Assert.assertEquals("db1", result.getString());
-		entityManager.flush();
+		if (!initialized) {
 
-	}
+			initialized = true;
 
-	// I don't really want this to be a test, just a before that does not run under a transaction, however, I can't make
-	// that work (@BeforeTransaction). This means that we must run all tests in the class.
-	@Rollback(false)
-	@Test
-	public void setupDB2() throws SQLException {
-		dataSource.getConnection().createStatement().execute("create schema " + DB2 + " " + CREATE_TABLE_STMT);
+			dataSource.getConnection().createStatement().execute("create schema " + DB1 + " " + CREATE_TABLE_STMT);
+			dataSource.getConnection().createStatement().execute("create schema " + DB2 + " " + CREATE_TABLE_STMT);
 
-		switchingDataSource.setDatabaseName(DB2);
-		entityManager.persist(getMockDomainObject("db2"));
-		DomainObject result = (DomainObject) entityManager.createQuery("select do from DomainObject do;")
-				.getResultList().get(0);
-		Assert.assertNotNull(result);
-		Assert.assertEquals("db2", result.getString());
-		entityManager.flush();
+			new TransactionTemplate(transactionManager).execute(new TransactionCallback<Void>() {
+				public Void doInTransaction(TransactionStatus status) {
+					setUpData(DB1, "db1");
+					return null;
+				}
+			});
+			new TransactionTemplate(transactionManager).execute(new TransactionCallback<Void>() {
+				public Void doInTransaction(TransactionStatus status) {
+					setUpData(DB2, "db2");
+					return null;
+				}
+			});
+
+		}
 
 	}
 
 	@Test
-	public void testGetFromD2() {
+	public void testGetFromDB2() {
 		switchingDataSource.setDatabaseName(DB2);
 		DomainObject result = (DomainObject) entityManager.createQuery("select do from DomainObject do;")
 				.getResultList().get(0);
@@ -88,6 +89,19 @@ public class DatabaseSwitchingDataSourceTest {
 	}
 
 	private static long nextId = 1;
+
+	private void setUpData(String databaseName, String objectName) {
+
+		switchingDataSource.setDatabaseName(databaseName);
+		DomainObject do1 = getMockDomainObject(objectName);
+		entityManager.persist(do1);
+		DomainObject result = (DomainObject) entityManager.createQuery("select do from DomainObject do;")
+				.getResultList().get(0);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(objectName, result.getString());
+		entityManager.flush();
+
+	}
 
 	private DomainObject getMockDomainObject(String data) {
 		DomainObject result = new DomainObject();
