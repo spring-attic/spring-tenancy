08b7f39ba4a1baee3d3051eb4d0057c9facace15
gonzalad <adr_gonzalez@yahoo.fr>
2017-08-06 21:06:45 +0200

    Support InheritableThreadLocal contextHolder
    Support an InheritableThreadLocal strategy for
    tenancyContext just as in Spring Security.

--- src/main/java/org/springframework/tenancy/context/TenancyContextHolder.java
+++ src/main/java/org/springframework/tenancy/context/TenancyContextHolder.java
@@ -20,6 +20,11 @@
 package org.springframework.tenancy.context;
 
 
+import java.lang.reflect.Constructor;
+
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
+
 /**
  * Associates a given {@link TenancyContext} with the current execution.
  * <p>
@@ -27,69 +32,146 @@ package org.springframework.tenancy.context;
  * The purpose of the class is to provide a convenient way to specify the strategy that should be used for a given JVM.
  * This is a JVM-wide setting, since everything in this class is <code>static</code> to facilitate ease of use in
  * calling code.
- * 
+ * <p>
+ * To specify which strategy should be used, you must provide a mode setting. A mode
+ * setting is one of the three valid <code>MODE_</code> settings defined as
+ * <code>static final</code> fields, or a fully qualified classname to a concrete
+ * implementation of
+ * {@link TenancyContextHolderStrategy} that provides a public no-argument constructor.
+ * <p>
+ * There are two ways to specify the desired strategy mode <code>String</code>. The first
+ * is to specify it via the system property keyed on {@link #SYSTEM_PROPERTY}. The second
+ * is to call {@link #setStrategyName(String)} before using the class. If neither approach
+ * is used, the class will default to using {@link #MODE_THREADLOCAL}, which is backwards
+ * compatible, has fewer JVM incompatibilities and is appropriate on servers (whereas
+ * {@link #MODE_GLOBAL} is definitely inappropriate for server use).
+ *
  * @author Clint Morgan (Tasktop Technologies Inc.)
- * 
  */
 public class TenancyContextHolder {
 
-	private static TenancyContextHolderStrategy strategy = new ThreadLocalTenancyContextHolderStrategy();
+    public static final String MODE_THREADLOCAL = "MODE_THREADLOCAL";
 
-	/**
-	 * Explicitly clear the tenacy context.
-	 * 
-	 */
-	public static void clearContext() {
-		strategy.clearContext();
-	}
+    public static final String MODE_INHERITABLETHREADLOCAL = "MODE_INHERITABLETHREADLOCAL";
 
-	/**
-	 * Obtain the current <code>TenancyContext</code>.
-	 * 
-	 * @return the tenancy context (never <code>null</code>)
-	 */
-	public static TenancyContext getContext() {
-		return strategy.getContext();
-	}
+    public static final String MODE_GLOBAL = "MODE_GLOBAL";
 
-	/**
-	 * Associates a new <code>TenancyContext</code> with the current context of execution.
-	 * 
-	 * @param context
-	 *            the new <code>TenancyContext</code> (may not be <code>null</code>)
-	 */
+    public static final String SYSTEM_PROPERTY = "spring.tenancy.strategy";
 
-	public static void setContext(TenancyContext context) {
-		strategy.setContext(context);
-	}
+    private static TenancyContextHolderStrategy strategy = new ThreadLocalTenancyContextHolderStrategy();
 
-	/**
-	 * Delegates the creation of a new, empty context to the configured strategy.
-	 */
-	public static TenancyContext createEmptyContext() {
-		return strategy.createEmptyContext();
-	}
+    private static String strategyName = System.getProperty(SYSTEM_PROPERTY);
 
-	/**
-	 * Allows retrieval of the context strategy.
-	 * 
-	 * @return the configured strategy for storing the tenancy context.
-	 */
-	public static TenancyContextHolderStrategy getStrategy() {
-		return strategy;
-	}
+    private static int initializeCount;
 
-	/**
-	 * Set the context strategy.
-	 * 
-	 * @param strategy
-	 *            the configured strategy for storing the tenancy context.
-	 */
-	public static void setStrategy(TenancyContextHolderStrategy strategy) {
-		if (strategy == null) {
-			throw new IllegalArgumentException();
-		}
-		TenancyContextHolder.strategy = strategy;
-	}
+    static {
+        initialize();
+    }
 
+    /**
+     * Explicitly clear the tenacy context.
+     */
+    public static void clearContext() {
+        strategy.clearContext();
+    }
+
+    /**
+     * Obtain the current <code>TenancyContext</code>.
+     *
+     * @return the tenancy context (never <code>null</code>)
+     */
+    public static TenancyContext getContext() {
+        return strategy.getContext();
+    }
+
+    /**
+     * Associates a new <code>TenancyContext</code> with the current context of execution.
+     *
+     * @param context the new <code>TenancyContext</code> (may not be <code>null</code>)
+     */
+    public static void setContext(TenancyContext context) {
+        strategy.setContext(context);
+    }
+
+    /**
+     * Delegates the creation of a new, empty context to the configured strategy.
+     */
+    public static TenancyContext createEmptyContext() {
+        return strategy.createEmptyContext();
+    }
+
+    /**
+     * Allows retrieval of the context strategy.
+     *
+     * @return the configured strategy for storing the tenancy context.
+     */
+    public static TenancyContextHolderStrategy getStrategy() {
+        return strategy;
+    }
+
+    /**
+     * Set the context strategy.
+     *
+     * @param strategy the configured strategy for storing the tenancy context.
+     */
+    public static void setStrategy(TenancyContextHolderStrategy strategy) {
+        if (strategy == null) {
+            throw new IllegalArgumentException();
+        }
+        TenancyContextHolder.strategy = strategy;
+    }
+
+    /**
+     * Primarily for troubleshooting purposes, this method shows how many times the class
+     * has re-initialized its <code>TenancyContextHolderStrategy</code>.
+     *
+     * @return the count (should be one unless you've called
+     * {@link #setStrategyName(String)} to switch to an alternate strategy.
+     */
+    public static int getInitializeCount() {
+        return initializeCount;
+    }
+
+    private static void initialize() {
+        if (!StringUtils.hasText(strategyName)) {
+            // Set default
+            strategyName = MODE_THREADLOCAL;
+        }
+
+        if (strategyName.equals(MODE_THREADLOCAL)) {
+            strategy = new ThreadLocalTenancyContextHolderStrategy();
+        } else if (strategyName.equals(MODE_INHERITABLETHREADLOCAL)) {
+            strategy = new InheritableThreadLocalTenancyContextHolderStrategy();
+        } else if (strategyName.equals(MODE_GLOBAL)) {
+            strategy = new GlobalTenancyContextHolderStrategy();
+        } else {
+            // Try to load a custom strategy
+            try {
+                Class<?> clazz = Class.forName(strategyName);
+                Constructor<?> customStrategy = clazz.getConstructor();
+                strategy = (TenancyContextHolderStrategy) customStrategy.newInstance();
+            } catch (Exception ex) {
+                ReflectionUtils.handleReflectionException(ex);
+            }
+        }
+
+        initializeCount++;
+    }
+
+    /**
+     * Changes the preferred strategy. Do <em>NOT</em> call this method more than once for
+     * a given JVM, as it will re-initialize the strategy and adversely affect any
+     * existing threads using the old strategy.
+     *
+     * @param strategyName the fully qualified class name of the strategy that should be
+     *                     used.
+     */
+    public static void setStrategyName(String strategyName) {
+        TenancyContextHolder.strategyName = strategyName;
+        initialize();
+    }
+
+    public String toString() {
+        return "TenancyContextHolder[strategy='" + strategyName + "'; initializeCount=" + initializeCount + "]";
+    }
 }
